41f1bf13491d9a45f77d83a7aa09d24c920d5783
laurattt <lauratoro422@gmail.com>
2026-01-09 21:13:57 +0100

    RELOAD PRACT (error con repositorio anterior)

---
+++ HIBERNATE-P1/src/main/java/com/jpaEjer1/Ciutada.java
@@ -0,0 +1,72 @@
+package com.jpaEjer1;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "CiutadaJPA")
+@Table(name = "CIUTADA_JPA") 
+public class Ciutada {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ciutadaId")
+    private long ciutadaId;
+    
+    @Column(name = "nom")
+    private String nom;
+    
+    @Column(name = "cognom")
+    private String cognom;
+    
+    @Column(name = "edat")
+    private int edat;
+
+    public Ciutada() {}
+
+    public Ciutada(String nom, String cognom, int edat) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.edat = edat;
+    }
+
+    public long getCiutadaId() { 
+        return ciutadaId; 
+    }
+
+    public void setCiutadaId(long ciutadaId) { 
+        this.ciutadaId = ciutadaId; 
+    }
+
+    public String getNom() { 
+        return nom; 
+    }
+
+    public void setNom(String nom) { 
+        this.nom = nom; 
+    }
+
+    public String getCognom() { 
+        return cognom; 
+    }
+
+    public void setCognom(String cognom) { 
+        this.cognom = cognom; 
+    }
+
+    public int getEdat() { 
+        return edat; 
+    }
+
+    public void setEdat(int edat) { 
+        this.edat = edat; 
+    }
+
+    @Override
+    public String toString() {
+        return "CiutadaJPA ID = " + ciutadaId + ", nom = " + nom + ", cognom = " + cognom + ", edat = " + edat ;
+    }
+}
---
+++ HIBERNATE-P1/src/main/java/com/xmlEjer0/Ciutada.java
@@ -0,0 +1,53 @@
+package com.xmlEjer0;
+
+public class Ciutada {
+    private long ciutadaId;
+    private String nom;
+    private String cognom;
+    private int edat;
+
+    public Ciutada() {}
+
+    public Ciutada(String nom, String cognom, int edat) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.edat = edat;
+    }
+
+    public long getCiutadaId() { 
+        return ciutadaId; 
+    }
+
+    public void setCiutadaId(long ciutadaId) { 
+        this.ciutadaId = ciutadaId; 
+    }
+
+    public String getNom() { 
+        return nom; 
+    }
+
+    public void setNom(String nom) { 
+        this.nom = nom; 
+    }
+
+    public String getCognom() { 
+        return cognom; 
+    }
+
+    public void setCognom(String cognom) { 
+        this.cognom = cognom; 
+    }
+
+    public int getEdat() { 
+        return edat; 
+    }
+
+    public void setEdat(int edat) { 
+        this.edat = edat; 
+    }
+
+    @Override
+    public String toString() {
+        return "Ciutada id =" + ciutadaId + ", nom = " + nom + ", cognom = " + cognom + ", edat = " + edat;
+    } 
+}
